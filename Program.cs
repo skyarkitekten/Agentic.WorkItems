d1daed4d1de1ed82375ec06c24d047afdfedb1f5
Chad Thomas <chad.c.thomas@gmail.com>
2025-09-03 08:30:41 -0400

    project init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.AI.Inference;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Immutable;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+var endpointUrl = new Uri(config["endpointUrl"] ?? "");
+
+var credential = new DefaultAzureCredential();
+
+AzureAIInferenceClientOptions clientOptions = new AzureAIInferenceClientOptions();
+BearerTokenAuthenticationPolicy tokenPolicy = new BearerTokenAuthenticationPolicy(
+    credential,
+    ["https://cognitiveservices.azure.com/.default"]
+);
+clientOptions.AddPolicy(tokenPolicy, HttpPipelinePosition.PerRetry);
+
+var client = new ChatCompletionsClient(
+    endpointUrl,
+    credential,
+    clientOptions
+);
